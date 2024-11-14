1dbd00aca1dd0933a187f90a6ee522596bc3f060
Michał Maciejewski <tmplt@tmplt.io>
2024-11-14 21:11:53 +0100

    Execution of home controlling in Main

--- ClimateController/Program.cs
+++ ClimateController/Program.cs
@@ -1,7 +1,31 @@
-﻿namespace ClimateController;
+using ClimateController.Configuration;
+using ClimateController.Home;
+using Microsoft.Extensions.Logging;
+
+namespace ClimateController;
 
 internal static class Program
 {
-    public static void Main()
-    { }
+    public static async Task Main()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSimpleConsole(options =>
+            {
+                options.SingleLine = true;
+            });
+            builder.SetMinimumLevel(Manager.LogLevel);
+        });
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        AppDomain.CurrentDomain.ProcessExit += (_, _) =>
+            cancellationTokenSource.Cancel();
+
+        var controller = Controller.Build(loggerFactory);
+        await controller.RunAsync(cancellationTokenSource.Token);
+
+        loggerFactory
+            .CreateLogger("Program")
+            .LogCritical("Unexpected end of one of tasks, finishing...");
+    }
 }
